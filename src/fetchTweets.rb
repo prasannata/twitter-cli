67e85ac6be558f3a6d17b217c765751935f6a893
Prasanna Thirumalai <prasanna.ta@gmail.com>
2012-05-23 23:25:57 -0700

    add http helper

--- src/fetchTweets.rb
+++ src/fetchTweets.rb
@@ -1,10 +1,25 @@
 #!/usr/bin/ruby
 
+require 'httpHelper'
+require 'tweet'
+
 class FetchTweets
   def initialize(user)
     @user = user
   end
   def execute
-    puts "fetching tweets for " + @user
+    params = {}
+    params['include_entities'] = 'true'
+    params['screen_name'] = "#{@user}"
+    params['count'] = '5'
+    
+    tweetsJson = HttpHelper.doGet('statuses/user_timeline.json', params)    
+    tweetsJson.each do |json|
+      tweet = Tweet.new
+      tweet.text = json['text']
+      puts tweet.to_s
+      puts "@" + json['user']['screen_name'].to_s	  
+      puts "-----------------------------------------------------------------------------------"      
+    end
   end
 end
--- src/httpHelper.rb
+++ src/httpHelper.rb
@@ -1,4 +1,20 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'rest_client'
+require 'json/pure'
+require 'addressable/uri'
+
 class HttpHelper
+
+    @@restService = 'https://api.twitter.com/1'
+    @@authorization = nil
+    @@header = {:accept => :json, :Authorization => @@authorization}
+    
+	def self.doGet(relativeUrl, queryParams)	
+	  uri = Addressable::URI.join(@@restService, relativeUrl)
+	  uri.query_values = queryParams
+	  response = RestClient.get uri.to_s, @@header
+	  return JSON.parse(response.body)    
+	end
 end
